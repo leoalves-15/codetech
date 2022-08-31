1d9f69163991eb88db81ebf3424f3496d1e718c8
leandroalves <leandro.alves@lighthouseit.com.br>
2022-08-31 20:14:42 -0300

    create colors

---
+++ src/Colors/Colors.ts
@@ -0,0 +1,17 @@
+export default {
+    CorLogo: '#C706FF',
+    CorLogo2: '##8964FF',
+    CorLogo3: '#AE7BFE',
+    CorLogo4: '#754DFD',
+    CorLogo5: '#A67AFC',
+    CorLogo6: '#D59AFE',
+    Banner: '#AE16D7',
+    CorBannerFundo:'#280034',
+    CorFundo: '#130118',
+    Botao:'linear-gradient(135deg, #12002B 0%, #A30E93 100%);',
+    FundoCompra: '#F1F1F1',
+    CorCard: 'transparent',
+    Card: '#EFF4F5',
+    BotaoMaster: 'linear-gradient(135deg, #3D033B 0%, #A30E93 100%);',
+    estrela: '#FFB800'
+};
