819ece3cc0813b936008dd5c49192ae68e68280f
leandroalves <leandro.alves@lighthouseit.com.br>
2022-09-05 18:34:13 -0300

    theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,13 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import storeConfig from "./store";
-<meta name="apple-mobile-web-app-status-bar-style theme-color" content="#C706FF" />
 
 const store = storeConfig();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
+    <meta name="apple-mobile-web-app-status-bar-style" content="#280034" />
+    <meta name="theme-color" content="#280034" />
     <React.StrictMode>
       <App />
     </React.StrictMode>
