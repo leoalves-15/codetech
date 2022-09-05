b447cbba62d6d69b66a59cccb2fff8662c69ec0e
leandroalves <leandro.alves@lighthouseit.com.br>
2022-09-05 17:16:45 -0300

    integration qtd

--- src/store/Reducers/Cart/index.ts
+++ src/store/Reducers/Cart/index.ts
@@ -5,7 +5,20 @@ const initial_value = cart || [{}];
 
 export default function reducer(state = initial_value, action) {
   if (action.type === "ADD_PRODUCT") {
-    let newCart = [...state, action.product]
+    let newCart = [...state]
+
+    const productInCartIndex = newCart.findIndex(item =>
+      item.id === action.product.id
+    );
+
+    if (productInCartIndex >= 0) {
+      newCart[productInCartIndex].qtd++;
+    } else {
+      newCart.push(
+          action.product
+      )
+    }
+
     localStorage.setItem("cart", JSON.stringify(newCart));
     return newCart;
   }
