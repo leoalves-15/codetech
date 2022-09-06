0dd9d92dde2ef02ff1c5367b11daf7e016aeb758
leandroalves <leandro.alves@lighthouseit.com.br>
2022-09-06 02:38:36 -0300

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./assets/fonts/Inter-3.19/InterWeb/inter.css";
-import Home from './pages/Home';
-import CartPage from './pages/Cart';
-import MenuPage from './pages/Menu';
+import Home from "./pages/Home";
+import CartPage from "./pages/Cart";
+import MenuPage from "./pages/Menu";
 
 function App() {
   return (
-    <div>
-      <Router>
-          <Routes>
-            <Route path="/Cart" element={<Home />}> </Route>
-             <Route path="/" element={<CartPage />}> </Route>
-            <Route path="/Menu" element={<MenuPage />}> </Route> 
-          </Routes>
-      </Router>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/Cart" element={<CartPage />}/>
+          <Route path="/Menu" element={<MenuPage />}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -2,7 +2,7 @@ import "../App.css";
 import "../assets/fonts/Inter-3.19/InterWeb/inter.css";
 import MobileMenu from "../components/MobileMenu";
 
-function MenuPage() {
+const MenuPage = () => {
 
   return (
     <div className="App">
